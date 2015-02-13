1972a36f1f672e7576d3836460d475957b6a7b7e
dzk <dzk@pc2-109-l.cs.st-andrews.ac.uk>
2015-02-13 17:44:13 +0000

    Missing files

---
+++ src/gui/DicePanel.java
@@ -0,0 +1,69 @@
+package gui;
+
+import game.Game;
+import game.Move;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+
+public class DicePanel extends JLabel {
+	private int color;
+	
+	public DicePanel(int color){
+		this.color = color;
+	}
+	
+	public void paintComponent(Graphics g){
+		if (Game.turn == this.color){
+			g.setColor(Color.black);
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			g.setColor(Color.white);
+			g.setFont(new Font(null, Font.PLAIN, 18));
+			int w = this.getWidth();
+			int h = this.getHeight();
+			switch(Move.dice[0]){
+				case 1:
+					g.drawString("1", 0, h);
+					break;
+				case 2:
+					g.drawString("2", 0, h);
+					break;
+				case 3:
+					g.drawString("3", 0, h);
+					break;
+				case 4:
+					g.drawString("4", 0, h);
+					break;
+				case 5:
+					g.drawString("5", 0, h);
+					break;
+				case 6:
+					g.drawString("6", 0, h);
+					break;
+			}
+			switch(Move.dice[1]){
+			case 1:
+				g.drawString("1", w/2, h);
+				break;
+			case 2:
+				g.drawString("2", w/2, h);
+				break;
+			case 3:
+				g.drawString("3", w/2, h);
+				break;
+			case 4:
+				g.drawString("4", w/2, h);
+				break;
+			case 5:
+				g.drawString("5", w/2, h);
+				break;
+			case 6:
+				g.drawString("6", w/2, h);
+				break;
+		}
+		}
+	}
+}
---
+++ src/gui/EndPanel.java
@@ -0,0 +1,34 @@
+package gui;
+
+import game.Piece;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class EndPanel extends ColumnPanel {
+
+	public EndPanel(int i, boolean faceDown) {
+		super(i, faceDown);
+	}
+
+	public void paintComponent(Graphics g){
+		g.setColor(Color.GRAY);
+		
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		for (int i = 0; i < column.getPieces().size(); i++){
+			if (column.getPieces().get(i).getColor() == Piece.WHITE){
+				g.setColor(Color.white);
+			} else {
+				g.setColor(Color.BLACK);
+			}
+			int y = 0;
+			if (this.faceDown){
+				y = this.getHeight()-(i+1)*size/5 - i;
+			} else {
+				y = i*size/5 + i;
+			}
+			g.fillRect(0, y, size, size/5);
+		} 
+	}
+	
+}
