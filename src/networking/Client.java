c7382dcffe04f65db5b85245cfa374b476fb7588
dzk <dzk@pc2-042-l.cs.st-andrews.ac.uk>
2015-02-18 16:31:41 +0000

    Networking...

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -5,15 +5,36 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client {
+public class Client extends Network {
 	public static void main(String[] args) throws UnknownHostException, IOException{
 		Socket call = new Socket("pc2-116-l", Server.port);
 		Scanner in = new Scanner(System.in);
+		
+		socketInput = call.getInputStream();
+		socketOutput = call.getOutputStream();
+		
+		if(!handshake(call)){
+			System.out.println("Handshake failed :(");
+			System.exit(-1);
+		} else {
+			System.out.println("Handshake succeeded! :)");
+		}
 		
 		String message;
 		while (!( message = in.nextLine()).equals("quit")){
 			call.getOutputStream().write(55);
 		}
 		
+	}
+	
+	public static boolean handshake(Socket other) throws IOException{
+		writeLine("hello");
+		if (readLine().equals("hello")){
+			writeLine("newgame");
+			if (readLine().equals("ready")){
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/networking/Server.java
+++ src/networking/Server.java
@@ -1,30 +1,54 @@
 package networking;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
-public class Server {
+public class Server extends Network {
 	public static final int port = 40013;
 	private static final int sleepTime = 200;
+	private static boolean isPlaying;
 	
 	public static void main(String[] args) throws IOException, InterruptedException{
 		ServerSocket serverSocket = new ServerSocket(port);
-		Socket clientSocket;
 		InetAddress h = InetAddress.getLocalHost();
 		String s = h.getCanonicalHostName();
 		System.out.println("host: " + h.getByName(s));
-		
+
 		System.out.println("Waiting for players...");
-		clientSocket = serverSocket.accept();
+		Socket clientSocket = serverSocket.accept();
+		
+		socketInput = clientSocket.getInputStream();
+		socketOutput = clientSocket.getOutputStream();
+		
+		System.out.println("Found player!");
+		
+		if (readLine().equals("hello")){
+			writeLine("hello");
+			if (readLine().equals("newgame")){
+				if (!isPlaying){
+					writeLine("ready");
+				} else {
+					writeLine("reject");
+				}
+			}
+		}
+		
+		
+		
+		
 		Scanner in = new Scanner(System.in);
+
 		
 		String message;
 		while (!( message = in.nextLine()).equals("quit")){
-			clientSocket.getOutputStream().write(56);
+			writeLine(message);
+			System.out.println(readLine());
 		}
 	}
+
 }
