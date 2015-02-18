ce24b930f64cc172e8e4ee6ed5b2dd03291d95c6
dzk <dzk@pc2-042-l.cs.st-andrews.ac.uk>
2015-02-18 17:51:38 +0000

    Networking works. Yay ._.

--- src/networking/Server.java
+++ src/networking/Server.java
@@ -1,14 +1,9 @@
 package networking;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server extends Network {
 	public static final int port = 40013;
