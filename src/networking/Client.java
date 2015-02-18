12bdcd62564967ad8324beb585cb34d551347493
dzk <dzk@pc2-116-l.cs.st-andrews.ac.uk>
2015-02-18 15:53:21 +0000

    Attempting networking

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -1,5 +1,19 @@
 package networking;
 
-public class Client {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class Client {
+	public static void main(String[] args) throws UnknownHostException, IOException{
+		Socket call = new Socket("pc2-116-l", Server.port);
+		Scanner in = new Scanner(System.in);
+		
+		String message;
+		while (!( message = in.nextLine()).equals("quit")){
+			call.getOutputStream().write(55);
+		}
+		
+	}
 }
--- src/networking/Server.java
+++ src/networking/Server.java
@@ -1,5 +1,30 @@
 package networking;
 
-public class Server {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
 
+public class Server {
+	public static final int port = 40013;
+	private static final int sleepTime = 200;
+	
+	public static void main(String[] args) throws IOException, InterruptedException{
+		ServerSocket serverSocket = new ServerSocket(port);
+		Socket clientSocket;
+		InetAddress h = InetAddress.getLocalHost();
+		String s = h.getCanonicalHostName();
+		System.out.println("host: " + h.getByName(s));
+		
+		System.out.println("Waiting for players...");
+		clientSocket = serverSocket.accept();
+		Scanner in = new Scanner(System.in);
+		
+		String message;
+		while (!( message = in.nextLine()).equals("quit")){
+			clientSocket.getOutputStream().write(56);
+		}
+	}
 }
